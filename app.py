e48bc384d146b5e63e15306b460f89d2bba20b25
MOD Administrator <admin@M365x519669.onmicrosoft.com>
2022-07-01 22:11:48 +0100

    first commit

---
+++ app.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask import request,Response,redirect
+from flask_caching import Cache
+from flask.json import jsonify
+import json
+import logging
+
+import sys, os, tempfile, uuid, time, datetime
+import configparser
+import argparse
+import requests
+from random import randint
+import msal
+from cryptography.x509 import load_pem_x509_certificate
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import hashes
+
+cacheConfig = {
+    "DEBUG": True,          # some Flask specific configs
+    "CACHE_TYPE": "SimpleCache",  # Flask-Caching related configs
+    "CACHE_DEFAULT_TIMEOUT": 300
+}
+app = Flask(__name__,static_url_path='',static_folder='static',template_folder='static')
+
+app.config.from_mapping(cacheConfig)
+cache = Cache(app)
+
+log = logging.getLogger() 
+log.setLevel(logging.INFO)
+
+with open('config.json') as f:
+    config = json.load(f)
+
+#config = json.load(open(sys.argv[1]))
+
+msalCca = msal.ConfidentialClientApplication( config["azClientId"], 
+    authority="https://login.microsoftonline.com/" + config["azTenantId"],
+    client_credential=config["azClientSecret"],
+    )
+
+if config["azCertificateName"] != "":
+    with open(config["azCertificatePrivateKeyLocation"], "rb") as file:
+        private_key = file.read()
+    with open(config["azCertificateLocation"]) as file:
+        public_certificate = file.read()
+    cert = load_pem_x509_certificate(data=bytes(public_certificate, 'UTF-8'), backend=default_backend())
+    thumbprint = (cert.fingerprint(hashes.SHA1()).hex())
+    print("Cert based auth using thumbprint: " + thumbprint)    
+    msalCca = msal.ConfidentialClientApplication( config["azClientId"], 
+       authority="https://login.microsoftonline.com/" + config["azTenantId"],
+        client_credential={
+            "private_key": private_key,
+            "thumbprint": thumbprint,
+            "public_certificate": public_certificate
+        }
+    )    
+
+# Check if it is an EU tenant and set up the endpoint for it
+r = requests.get("https://login.microsoftonline.com/" + config["azTenantId"] + "/v2.0/.well-known/openid-configuration")
+resp = r.json()
+print("tenant_region_scope = " + resp["tenant_region_scope"])
+config["tenant_region_scope"] = resp["tenant_region_scope"]
+config["msIdentityHostName"] = "https://beta.did.msidentity.com/v1.0/"
+if resp["tenant_region_scope"] == "EU":
+    config["msIdentityHostName"] = "https://beta.eu.did.msidentity.com/v1.0/"
+
+# Check that the Credential Manifest URL is in the same tenant Region and throw an error if it's not
+if False == config["CredentialManifest"].startswith( config["msIdentityHostName"] ):
+    raise ValueError("Error in config file. CredentialManifest URL configured for wrong tenant region. Should start with: " + config["msIdentityHostName"])
+    
+import issuer
+import verifier
+
+@app.route('/')
+def root():
+    return app.send_static_file('index.html')
+
+@app.route("/echo", methods = ['GET'])
+def echoApi():
+    result = {
+        'date': datetime.datetime.utcnow().isoformat(),
+        'api': request.url,
+        'Host': request.headers.get('host'),
+        'x-forwarded-for': request.headers.get('x-forwarded-for'),
+        'x-original-host': request.headers.get('x-original-host')
+    }
+    return Response( json.dumps(result), status=200, mimetype='application/json')
+
+if __name__ == "__main__":
+    app.run()
---
+++ issuer.py
@@ -0,0 +1,125 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask import request,Response,redirect
+from flask_caching import Cache
+from flask.json import jsonify
+import json
+import logging
+import sys, os, tempfile, uuid, time, datetime
+import configparser
+import argparse
+import requests
+from random import randint
+import msal
+from app import app
+from app import cache
+from app import log
+from app import config
+from app import msalCca
+#from __main__ import app
+#from __main__ import cache
+#from __main__ import log
+#from __main__ import config
+#from __main__ import msalCca
+
+#fI = open(sys.argv[2],)
+#issuanceConfig = json.load(fI)
+#fI.close()  
+
+with open('issuance_request_config.json') as f:
+    issuanceConfig = json.load(f)
+    
+
+apiKey = str(uuid.uuid4())
+
+issuanceConfig["callback"]["headers"]["api-key"] = apiKey
+issuanceConfig["authority"] = config["IssuerAuthority"]
+issuanceConfig["issuance"]["manifest"] = config["CredentialManifest"]
+if "pin" in issuanceConfig["issuance"] is not None:
+    if int(issuanceConfig["issuance"]["pin"]["length"]) == 0:
+        del issuanceConfig["issuance"]["pin"]
+
+@app.route("/api/issuer/issuance-request", methods = ['GET'])
+def issuanceRequest():
+    """ This method is called from the UI to initiate the issuance of the verifiable credential """
+    id = str(uuid.uuid4())
+    accessToken = ""
+    result = msalCca.acquire_token_for_client( scopes="3db474b9-6a0c-4840-96ac-1fceb342124f/.default" )
+    if "access_token" in result:
+        print( result['access_token'] )
+        accessToken = result['access_token']
+    else:
+        print(result.get("error") + result.get("error_description"))
+
+    payload = issuanceConfig.copy()
+    payload["callback"]["url"] = str(request.url_root).replace("http://", "https://") + "/api/issuer/issuance-request-callback"
+    payload["callback"]["state"] = id
+    pinCode = 0
+    if "pin" in payload["issuance"] is not None:
+        pinCode = ''.join(str(randint(0,9)) for _ in range(int(payload["issuance"]["pin"]["length"])))
+        payload["issuance"]["pin"]["value"] = pinCode
+    if "claims" in payload["issuance"] is not None:
+        payload["issuance"]["claims"]["given_name"] = "Megan"
+        payload["issuance"]["claims"]["family_name"] = "Bowen"
+    print( json.dumps(payload) )
+    post_headers = { "content-type": "application/json", "Authorization": "Bearer " + accessToken }
+    client_api_request_endpoint = config["msIdentityHostName"] + config["azTenantId"] + "/verifiablecredentials/request"
+    r = requests.post( client_api_request_endpoint
+                    , headers=post_headers, data=json.dumps(payload))
+    resp = r.json()
+    print(resp)
+    resp["id"] = id
+    if "pin" in payload["issuance"] is not None:
+        resp["pin"] = pinCode
+    return Response( json.dumps(resp), status=200, mimetype='application/json')
+
+@app.route("/api/issuer/issuance-request-callback", methods = ['POST'])
+def issuanceRequestApiCallback():
+    """ This method is called by the VC Request API when the user scans a QR code and presents a Verifiable Credential to the service """
+    issuanceResponse = request.json
+    print(issuanceResponse)
+    if request.headers['api-key'] != apiKey:
+        print("api-key wrong or missing")
+        return Response( jsonify({'error':'api-key wrong or missing'}), status=401, mimetype='application/json')
+    if issuanceResponse["code"] == "request_retrieved":
+        cacheData = {
+            "status": issuanceResponse["code"],
+            "message": "QR Code is scanned. Waiting for issuance to complete..."
+        }
+        cache.set( issuanceResponse["state"], json.dumps(cacheData) )
+        return ""
+    if issuanceResponse["code"] == "issuance_successful":
+        cacheData = {
+            "status": issuanceResponse["code"],
+            "message": "Credential successfully issued"
+        }
+        cache.set( issuanceResponse["state"], json.dumps(cacheData) )
+        return ""
+    if issuanceResponse["code"] == "issuance_error":
+        cacheData = {
+            "status": issuanceResponse["code"],
+            "message": issuanceResponse["error"]["message"]
+        }
+        cache.set( issuanceResponse["state"], json.dumps(cacheData) )
+        return ""
+    return ""
+
+@app.route("/api/issuer/issuance-response", methods = ['GET'])
+def issuanceRequestStatus():
+    """ this function is called from the UI polling for a response from the AAD VC Service.
+    when a callback is recieved at the presentationCallback service the session will be updated
+     """
+    id = request.args.get('id')
+    print(id)
+    data = cache.get(id)
+    print(data)
+    if data is not None:
+        cacheData = json.loads(data)
+        browserData = {
+            'status': cacheData["status"],
+            'message': cacheData["message"]
+        }
+        return Response( json.dumps(browserData), status=200, mimetype='application/json')
+    else:
+        return ""
